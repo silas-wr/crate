66f667bd27b3750c8b52164f3226ba3bac6042bf
Silas Wrinker <alcosojr79@gmail.com>
2024-06-12 13:47:42 -0400

    Update compile.sh
    updated for github repository.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 javac com/crate/Main.java
-jar --create --manifest=com/crate/META-INF/MANIFEST.MF --file=lib/crate.jar
 gzip man/man1/crate.1 -kf19
-native-image com.crate.Main -o bin/crate
