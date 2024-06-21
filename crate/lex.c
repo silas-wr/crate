45fd07c2096b4c59ac890a7e3c14559e43f4dc04
Silas Wrinker <alcosojr79@gmail.com>
2024-06-21 14:46:57 -0400

    Update lex.c

--- crate/lex.c
+++ crate/lex.c
@@ -3,14 +3,21 @@
 struct Tokens lex(char src[])
 {
   int  i = 0;
+  int  t = 0;
   char c;
-  
+
+  struct Token  newtok;
   struct Tokens tlist;
   
   while ((c = src[i++]) != EOF)
   {
     printf("%c", c);
+    
+    newtok = { 0, "Hi" };
+    tlist.tlist[i++] = newtok;
   }
+
+  tlist.exitcode = 0;
 
   return tlist;
 }
