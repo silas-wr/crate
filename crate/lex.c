58817903de1989e9fb57b5e0f421db73c48ad0c7
Silas Wrinker <alcosojr79@gmail.com>
2024-06-21 15:02:53 -0400

    Update lex.c

--- crate/lex.c
+++ crate/lex.c
@@ -11,7 +11,31 @@ struct Tokens lex(char src[])
   
   while ((c = src[i++]) != EOF)
   {
-    printf("%c", c);    
+    switch (c)
+    {
+      case 'a': case 'b': case 'c': case 'd' :case 'e': case 'f': case  'g': case 'h': case 'i': case 'j': 
+      case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case  'q': case 'r': case 's': case 't': 
+      case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': case  'A': case 'B': case 'C': case 'D': 
+      case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case  'K': case 'L': case 'M': case 'N': 
+      case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case  'U': case 'V': case 'W': case 'X': 
+      case 'Y': case 'Z': 
+        printf("alphabetical");
+        break;
+
+      case '{': case '[': case '(': case ')': case ']': case '}': case ';': 
+      case '#': case '\n': case '\t': case '\r': case ' ':
+        printf("whitespace");
+        break;
+
+      case '\0':
+        printf("EOF");
+        break;
+
+      default:
+        printf("unknown");
+        break;
+    }
+    
     t++;
   }
 
