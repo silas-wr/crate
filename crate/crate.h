bd890838659c5ae26afc043b5145b2c2ad4dccaf
Silas Wrinker <alcosojr79@gmail.com>
2024-06-21 14:15:35 -0400

    Update crate.h

--- crate/crate.h
+++ crate/crate.h
@@ -1,2 +1,22 @@
 #include <stdio.h>
 #include <stdarg.h>
+
+struct Token {
+  int ttype;
+  char value[];
+};
+
+struct Node {
+  struct Token tokens[];
+  int ntype;
+};
+
+struct Program {
+  struct Node nodes[];
+};
+
+struct Token lex(char name[])[];
+struct Program parse(struct Token tlist[]);
+int compile(struct Program parsed);
+int run(char file[]);
+int strparse(char s[]);
