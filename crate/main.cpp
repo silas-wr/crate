6ea6dc9f2f9ee3974fe23c4c326acec3eff4dc8f
Silas Wrinker <alcosojr79@gmail.com>
2024-06-27 12:59:28 -0400

    Update main.cpp

--- crate/main.cpp
+++ crate/main.cpp
@@ -1,9 +1,13 @@
 #include "crate.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
   std::string sample = "hi! this is a test";
   
   std::vector<Token> tokens = lex(sample);
+
+  for(const Token& tk : tokens) 
+    cout << "Token = " << tk.ttype << ", " << tk.value;
+  
   return 0;
 }
