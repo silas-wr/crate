6fc65e0360c055d309ded7c4b37b24ec7ae486ef
Silas Wrinker <alcosojr79@gmail.com>
2024-06-27 13:40:17 -0400

    Update lex.cpp

--- crate/lex.cpp
+++ crate/lex.cpp
@@ -7,34 +7,31 @@ std::vector<Token> lex(std::string src)
 
   std::vector<Token> tlist;
   Token cur;
+
+  std::string alphabet = "abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  std::string numeric = "0123456789";
+  std::string operators = "&|^~!=<>:+-*/";
+  std::string white = "{[(#?;)]}\r\t\n ";
   
   for (c = src[i]; i < src.size(); i++) {
-    switch (c)
-    {
-      case 'a': case 'b': case 'c': case 'd' :case 'e': case 'f': case  'g': case 'h': case 'i': case 'j': 
-      case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case  'q': case 'r': case 's': case 't': 
-      case 'u': case 'v': case 'w': case 'x': case 'y': case 'z': case  'A': case 'B': case 'C': case 'D': 
-      case 'E': case 'F': case 'G': case 'H': case 'I': case 'J': case  'K': case 'L': case 'M': case 'N': 
-      case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case  'U': case 'V': case 'W': case 'X': 
-      case 'Y': case 'Z': 
-        std::cout << "alphabetical\n";
-        break;
-
-      case '{': case '[': case '(': case ')': case ']': case '}': case ';': 
-      case '#': case '\n': case '\t': case '\r': case ' ':
-        std::cout << "whitespace\n";
-        break;
-      
-      default:
-        std::cout << "unknown\n";
-        break;
+    if (alphabet::find(c) != -1) {
+      std::cout << "alphabetical";
+    } else if (numeric::find(c) != -1) {
+      std::cout << "numeric";
+    } else if (operators::find(c) != -1) {
+      std::cout << "operational";
+    } else if (white::find(c) != -1) {
+      std::cout << "whitespace";
+    } else {
+      std::cout << "unknown";
     }
-
-    cur.ttype = EOF;
-    cur.value = "";
-    
-    tlist.push_back(cur);
   }
+
+  
+  cur.ttype = EOF;
+  cur.value = "";
+    
+  tlist.push_back(cur);
 
   return tlist;
 }
