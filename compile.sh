e70322aa2ef33c0d60496f634a591ec3d2f754b9
Silas Wrinker <alcosojr79@gmail.com>
2024-06-28 14:07:31 -0400

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc crate/main.cpp crate/lex.cpp -o crate/crate -lstdc++
-./crate/crate
+gcc crate/*.cpp -o crate/crate -lstdc++
+./crate/crate sample.crate
 gzip man/man1/crate.1 -kf19
 man -l man/man1/crate.1.gz
