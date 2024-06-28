d5ee33dd4fd9d94b60acacbc2cc151fed65fe0bb
Silas Wrinker <alcosojr79@gmail.com>
2024-06-28 14:01:51 -0400

    Update main.cpp

--- crate/main.cpp
+++ crate/main.cpp
@@ -2,12 +2,34 @@
 
 int main(int argc, char* argv[])
 {
-  std::string sample = "hi! this is a test";
+  if (argc != 2) {
+    cout << "Usage: crate FILE";
+    exit(1);
+  } else {
+    string fname = argv[1];
+  }
+
+  string content = "";
+  string line;
   
-  std::vector<Token> tokens = lex(sample);
+  ifstream fi;
+  fi.open(fname);
+  
+  if (fi.is_open()) {
+    while (getline(fi,line)){
+      content += line;
+      content += '\n';
+    }
+    myfile.close();
+  } else {
+    cout << "[!] Unable to open file.";
+    exit(1);
+  }
+  
+  vector<Token> tokens = lex(sample);
 
   for(const Token& tk : tokens) 
-    std::cout << "Token = " << tk.ttype << ", " << tk.value << "\n";
+    cout << "Token = " << tk.ttype << ", " << tk.value << "\n";
   
   return 0;
 }
