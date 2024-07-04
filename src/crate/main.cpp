ec921c15976549f871077e799ba7578227b759b7
Silas Wrinker <alcosojr79@gmail.com>
2024-07-04 16:49:02 -0400

    Update main.cpp

--- src/crate/main.cpp
+++ src/crate/main.cpp
@@ -7,19 +7,16 @@ int main(int argc, char* argv[])
   if (argc != 2) {
     cout << "Usage: crate FILE";
     exit(1);
-  } else {
-    string fname = argv[1];
-  }
 
   string content = "";
   string line;
   
-  ifstream fi(fname);
+  ifstream fi(argv[1]);
 
   if (fi.is_open()) {
-    cout << "File '" << fname << "' is now open.";
+    cout << "File '" << argv[1] << "' is now open.";
   } else {
-    cout << "File '" << fname << "' could not be opened.";
+    cout << "File '" << argv[1] << "' could not be opened.";
   }
   
   while (getline(fi,line)){
