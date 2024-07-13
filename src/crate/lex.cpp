770e62a11c7ea595e72937ed9f0e681170ff626f
Silas Wrinker <alcosojr79@gmail.com>
2024-07-13 16:14:05 -0400

    Update lex.cpp

--- src/crate/lex.cpp
+++ src/crate/lex.cpp
@@ -18,20 +18,91 @@ vector<Token> lex(string src)
   string alphabet = "abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   string numeric = "0123456789";
   string operators = "&|^~!=<>:+-*/";
-  string white = "{[(#?;)]}\r\t\n ";
+  string white = "{[(#?,;)]}\r\t\n ";
 
   map<string, TokenTypes> keys;
 
+  // keywords
   keys["import"] = IMPORT;
+
+  // control
+  keys["if"] = IF;
+  keys["elif"] = ELIF;
+  keys["else"] = ELSE;
+  keys["switch"] = SWITCH;
+  keys["case"] = CASE;
+  keys["default"] = DEFAULT;
+  keys["try"] = TRY;
+  keys["except"] = EXCEPT;
+  keys["while"] = WHILE;
+  keys["break"] = BREAK;
+  keys["continue"] = CONTINUE;
+
+  // types
+  keys["const"] = TCNT;
+  keys["void"] = TNUL;
+  keys["chr"] = TCHR;
+  keys["byte"] = TBYT;
+  keys["short"] = TSHR;
   keys["int"] = TINT;
+  keys["long"] = TLNG;
+  keys["double"] = TFLT;
   keys["type"] = TTYP;
 
   map<string, TokenTypes> ops;
 
+  // assignment
   ops[":"] = IS;
   ops["::"] = ATTR;
+  ops[":="] = ENUM;
   ops["<<"] = TYPE;
+  ops[">>"] = RTYP;
 
+  // math
+  ops["+"] = SUM;
+  ops["-"] = SUB;
+  ops["*"] = MUL;
+  ops["/"] = DIV;
+  ops["%"] = MOD;
+  ops["**"] = EXP;
+
+  // bin
+  ops["&"] = AND;
+  ops["|"] = OR;
+  ops["^"] = XOR;
+  ops["~"] = NOT;
+  ops["~>"] = RSH;
+  ops["<~"] = LSH;
+
+  // compound assignment
+  ops["++"] = INC;
+  ops["--"] = DEC;
+  ops["+:"] = PLEQ;
+  ops["-:"] = SBEQ;
+  ops["*:"] = MLEQ;
+  ops["/:"] = DVEQ;
+  ops["%:"] = MDEQ;
+  ops["**:"] = EXEQ;
+  ops["&:"] = ANEQ;
+  ops["|:"] = OREQ;
+  ops["^:"] = XREQ;
+  ops["~:"] = NTEQ;
+  ops[">:"] = RSEQ;
+  ops["<:"] = LSEQ;
+
+  // conditional
+  ops["&&"] = AAND;
+  ops["||"] = OOR;
+  
+  // comparison
+  ops[">="] = GE;
+  ops["=<"] = LE;
+  ops["=="] = EQ;
+  ops["!="] = NEQ;
+
+  // generics
+  ops["<"] = LAB;
+  ops[">"] = RAB;
   
   for (int i = 0; i < src.size(); i++) {
 
@@ -80,9 +151,20 @@ vector<Token> lex(string src)
         load_type = "alpha";
         load_var = "";
         load_var += c;
+      } else if (load_type == "period") {
+        cur.ttype = ARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+      } else if (load_type == "splat") {
+        cur.ttype = KWARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+
+        load_type = "";
+        load_var = "";
       } else {
         cout << "[" << row << ", " << col << "] we're so sorry. something went wrong with the lexical analyzer. \n\tplease notify me at silas-wr/crate on github.\n";
-        ok = 0; // make it uncompilable    
+        ok = false; // make it uncompilable    
         load_type = "";
         load_var = "";
       }
@@ -94,6 +176,31 @@ vector<Token> lex(string src)
         load_var += c;
       } else if (load_type == "alpha") {
         load_var += c;
+      } else if (load_type == "operational") {
+        if (ops.find(load_var) != ops.end()) {
+          cur.ttype = ops[load_var];
+          cur.value = load_var;
+          tlist.push_back(cur);
+        } else {
+          cout << "[" << row << ", " << col << "] Invalid operator.\n";
+          ok = false;
+        }
+      } else if (load_type == "period") {
+        load_type = "float";
+        load_var += c;
+      } else if (load_type == "splat") {
+        cur.ttype = KWARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+
+        load_type = "int";
+        load_var = "";
+        load_var += c;
+      } else {
+        cout << "[" << row << ", " << col << "] we're so sorry. something went wrong with the lexical analyzer. \n\tplease notify me at silas-wr/crate on github.\n";
+        ok = false; // make it uncompilable    
+        load_type = "";
+        load_var = "";
       }
     }
     // operators
@@ -120,10 +227,37 @@ vector<Token> lex(string src)
         cur.ttype = FLOAT;
         cur.value = load_var;
       }
+
+      if (load_type == "period") {
+        cur.ttype = ARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+      }
+      
+      if (load_type == "splat") {
+        cur.ttype = KWARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+      }
      
       load_type = "operational";  
       load_var = "";
       load_var += c;
+    }
+    // periods
+    else if (c == '.') {
+      if (load_type == "") {
+        load_type = "period";
+        load_var += c;
+      } else if (load_type == "int") {
+        load_type = "float";
+        load_var += c;
+      } else if (load_type == "float") {
+        cout << "[" << row << ", " << col << "] Invalid operator.\n";
+        ok = false;
+        load_type = "";
+        load_var = "";
+      }
     }
     // whitespace
     else if (white.find(c) != -1) {  
@@ -155,10 +289,20 @@ vector<Token> lex(string src)
         } else {
           cout << "[" << row << ", " << col << "] Invalid operator.\n";
           ok = false;
+          load_type = "";
+          load_var = "";
         }
+      } else if (load_type == "period") {
+        cur.ttype = ARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
+      } else if (load_type == "splat") {
+        cur.ttype = KWARGS;
+        cur.value = load_var;
+        tlist.push_back(cur);
       } else {
         cout << "[" << row << ", " << col << "] we're so sorry. something went wrong with the lexical analyzer. \n\tplease notify me at silas-wr/crate on github.\n";
-        ok = false; // make it uncompilable    
+        ok = false; // make it uncompilable
         load_type = "";
         load_var = "";
       }
@@ -212,6 +356,10 @@ vector<Token> lex(string src)
           cur.value = c;
           tlist.push_back(cur);
           break;
+        case ',':
+          cur.ttype = COMM;
+          cur.value = c;
+          tlist.push_back(cur);
         default:
           break;
       }
