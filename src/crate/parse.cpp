cebba2c9ea66c873ead7a92fe08ffbae8822839f
Silas Wrinker <alcosojr79@gmail.com>
2024-07-17 17:25:17 -0400

    Update parse.cpp

--- src/crate/parse.cpp
+++ src/crate/parse.cpp
@@ -1,7 +1,43 @@
 #include <crate.hpp>
 
-Program parse(vector<Token>) {
+Program parse(vector<Token> tlist) {
   Program nlist;
+  Node cur;
+  Token tok;
+  
+  bool eol = true;
+  bool eof = true;
+  bool complex = false;
+  bool ok = true;
 
-  return nlist;
+  string load_type;
+  vector<Token> load_var;
+  vector<Node> nodes;
+
+  for (int i = 0; i < size(tlist); i++) {
+    tok = tlist.at(i);
+
+    if (tok.ttype == EOL) {
+      if (eol) {
+        ;
+      } else {
+        cout << "[] " << "Unexpected EOL";
+        ok = false;
+      }
+    } else if (tok.ttype == EOF) {
+      if (eof) {
+        ;
+      } else {
+        cout << "[] " << "Unexpected EOF";
+        ok = false;
+      }
+    }
+  }
+
+  if (ok) {
+    nlist.nodes = nodes;
+    return nlist; 
+  } else {
+    exit(1);
+  }
 }
