10671fbec97d2dabd0c89ec6cdaff4ade8fba29d
Silas Wrinker <alcosojr79@gmail.com>
2024-07-20 17:28:24 -0400

    Update extraparse.cpp

--- src/crate/extraparse.cpp
+++ src/crate/extraparse.cpp
@@ -1,15 +1,15 @@
 #include "crate.hpp"
 
-void nodes(Node n, int tab) {
-  cout << string(tab, '\t') << "Node " << node.ntype << ": \n";
-  tab++;
+void nodes(Node n, int tb) {
+  cout << string(tb, '\t') << "Node " << n.ntype << ": \n";
+  tb++;
   for (int i = 0; i < n.nodes.size(); i++) {
     for (int j = 0; j < n.tokens.size(); i++) {
-      cout << string(tab, '\t') << " Token " << n.tokens.at(j).ttype << ": " << n.tokens.at(j).value << "\n";
+      cout << string(tb, '\t') << " Token " << n.tokens.at(j).ttype << ": " << n.tokens.at(j).value << "\n";
     }
-    nodes(n.nodes.at(i));
+    nodes(n.nodes.at(i), tb);
   }
-  tab--;
+  tb--;
 }
 
 void prog(Program p) {
